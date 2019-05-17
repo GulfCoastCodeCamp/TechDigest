8c47c4e1271af0f82e0d23a4733a97cce0d1b207
Xiangdong Zhang <zhangxd6@hotmail.com>
2019-05-16 23:07:51 -0500

    add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,58 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allArticles } }) => {
+              return allArticles.edges.map(edge => {
+                return Object.assign({}, {}, {
+                  description: edge.node.Description,
+                  date: edge.node.CreateTime,
+                  url: edge.node.Link,
+                  guid: edge.node.RowKey,
+                  custom_elements: [{ "tags": edge.node.Tags }],
+                  title:edge.node.Title
+                })
+              })
+            },
+            query: `
+              {
+                allArticles(sort: { fields: [CreateTime], order: DESC }) {
+                  edges {
+                    node {
+                      Title
+                      Description
+                      CreateTime(formatString: "MMMM DD, YYYY")
+                      Link
+                      Tags
+                      PartitionKey
+                      RowKey
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Tech Digiest's RSS Feed",
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
